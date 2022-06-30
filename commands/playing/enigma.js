c8e50627505c2b0483229b42527480a8c0ba4f74
DevPepesito <86423289+DevPepesito@users.noreply.github.com>
2022-06-30 17:59:25 +0200

    Big Update
    I got an idea of a better way to build the rotors and reflectors
    
    Some improvements in the code, Less lines to make the same things
    
    Trying to make the system to cycle the rotors
    
    Real upload date: Thu Jun 30 17:59:25 2022 +0200

--- commands/playing/enigma.js
+++ commands/playing/enigma.js
@@ -1,13 +1,6 @@
-const ro1 = require('../../lib/objects/enigma/rotores/I.js');
-const ro2 = require('../../lib/objects/enigma/rotores/II.js');
-const ro3 = require('../../lib/objects/enigma/rotores/III.js');
-const ro4 = require('../../lib/objects/enigma/rotores/IV.js');
-const ro5 = require('../../lib/objects/enigma/rotores/V.js');
-const ro6 = require('../../lib/objects/enigma/rotores/VI.js');
-const ro7 = require('../../lib/objects/enigma/rotores/VII.js');
-const ro8 = require('../../lib/objects/enigma/rotores/VIII.js');
-const reB = require('../../lib/objects/enigma/reflectores/B.js');
+const { RO, RE, t, g, spin } = require('../../lib/objects/enigma/pieces.js')
 const tr = require('../../lib/objects/traductor.js');
+
 module.exports = {
   name: 'enigma',
   aliases: ['en', 'enc'],
@@ -25,30 +18,32 @@ module.exports = {
     /* Que debo hacer?
         D Primero que todo se recibe el mensaje, cada caracter debe procesarse por medio de los rotores
         D Cada rotor tendrá una convinacion aleatoria hecha por mi
-        - D El programa default tendra 3 rotores comunes, se pueden añadir diferentes rotores hasta 10, mas son con privilegios
+        - D El programa default tendra 3 rotores comunes, se pueden añadir diferentes rotores hasta 10, más = con privilegios
         - D El ultimo rotor devolverá la señal de nuevo a los 3 rotores de forma inversa
         - PPP Cada rotor debe tener un pin programable para que su constante giro sea desde una posicion mas arriba de la anterior
         - PPP Una vez la señal pasa por todos los rotores se dirige al panel donde se podrá llegar a cambiar el resultado de los rotores
-            por el resultado al cual se le asigne, como un rotor configurable
+          por el resultado al cual se le asigne, como un rotor configurable
         D Los espacios no se codifican
-        PPP Los rotores deben girar cada ciclo y girar 1+ el rotor siguiente
+        - PPP Los rotores deben girar cada ciclo y girar 1+ el rotor siguiente
 
     */
     let inT = text.toLowerCase().split(""); // Entrada por ahora no hay programas
-    let machine = {
+    let machine = { // This machine is just a deffault schema to test and use
       rotors: [
-        ro1,
-        ro2,
-        ro3
+        RO.r1,
+        RO.r2,
+        RO.r3
       ],
-      reflector: reB,
+      reflector: RE.r1,
     };
     function run(txt) {
+
       let outT = ""; // Output variable
       let rotors = machine.rotors.length; // Amount of rotors that user has
       for (char of txt) { // Iterates all characters even spaces
+
         if (char === " ") { outT += " "; continue; }; // Doesnt translate spaces
-        let tempL = tr[char]; // Translates the letter to number
+        let tempL = tr[char.toUpperCase()]; // Translates the letter to number
         for (let j = 0; j < rotors * 2 + 1; j++) { // Cycles trough rotors and reflectors
           let cycles = rotors - Math.abs(j - rotors); // 0, 1, 2, ..n, reflector, n.., 2, 1, 0
           if (j === rotors) { // The middle cycle when we use the reflector
@@ -57,12 +52,12 @@ module.exports = {
 
             /* console.log("REF:", cycles, j, tr[tempL], ": " + tr[machine.reflector[tempL]]); */
           } else if (j > rotors) { // Turning cycles when the signal is going backwards trough the rotors
-            tempL = machine.rotors[cycles].t(tempL);
+            tempL = t(tempL, machine.rotors[cycles]);
 
 
             /* console.log("ROT~:", [cycles], j, tr[tempL], ": " + tempL); */
           } else if (j < rotors) { // Going cycles when the signal is going forward trough the rotors
-            tempL = machine.rotors[cycles].g(tempL);
+            tempL = g(tempL, machine.rotors[cycles]);
 
 
             /* console.log("ROT:", cycles, [j], tr[tempL], ": " + tempL); */
@@ -71,17 +66,17 @@ module.exports = {
 
           /* console.log(char.toUpperCase(), "| " + tr[tempL], ">>", cycles, j + 1); */
         }
-
-        outT += tr[tempL];
+        outT += tr[tempL]
+        //spin(machine.rotors)
       }
       return outT;
     };
     if (test_) {
       let res = run(inT).toUpperCase()
-      console.log(res);
-      return res;
+      console.log(res)
+      return res
     }
-    message.channel.send(run(inT).toUpperCase());
+    message.channel.send(run(inT).toUpperCase())
   }
 };
 
