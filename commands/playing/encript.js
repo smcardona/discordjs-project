44bd803a5eb162f1e3e9779a94edb29852f01d2b
DevPepesito <86423289+DevPepesito@users.noreply.github.com>
2022-07-20 15:04:36 +0200

    New random rotor generator
    I have been working on a order randomizer that creates random rotors depending on a seed, so It's not totally random.
    To make these generator I used a cool library called random-seed, Its truly useful.
    Also improved some things on the random functions
    My plans for the future is to update the bot to v13, and stop using v12, Im not saying v12 is bad but I just want to learn and try slash commands and new cool features in v13
    Next commit will be probably making the pin system or filter on the output or anything that comes to my mind.

--- commands/playing/encript.js
+++ commands/playing/encript.js
@@ -9,6 +9,7 @@ module.exports = {
   expectedArgs: '[+1 characters]',
   async execute({ message, text }, _test) {
     /* Que debo hacer?
+    PPP : Pending        D : Done          
         D Primero que todo se recibe el mensaje, cada caracter debe procesarse por medio de los rotores
         D Cada rotor tendrá una convinacion aleatoria hecha por mi
         D El programa default tendra 3 rotores comunes, se pueden añadir diferentes rotores hasta 10, más = con privilegios
@@ -19,7 +20,10 @@ module.exports = {
         D Los espacios no se codifican
         D Los rotores deben girar cada ciclo y girar 1+ el rotor siguiente 
         D Los rotores se podran posicionar y ordenar antes de utilizarse
-        - PPP Incorporación de usuarios, contraseñas de desencriptado e instrucciones
+        - PPP Incorporación de usuarios e instrucciones
+        D Contraseñas de desencriptado 
+        - PPP La salida del encriptado debe ser dividida en grupos de una cantidad de caracteres aleatoria
+        - PPP La salida de un desencriptado debe ser unida y luego remplazar el grupo de letras que simbolicen espacios [DEBO PENSARLO]
 
     */
     let noset, inT, sets = {};
@@ -31,20 +35,20 @@ module.exports = {
     } else { inT = text.toUpperCase() }
 
     /* Config schema should be like:
-      Sections => '[Used rotors order, last is refl: BACDa] | [Pin sets: TRDC] | [[Start k: KSFT], <Type k: LKMN>] | text'
-      String => 'BACDA TRDC KSFT LKMN | text'
+      Sections => '<Used rotors order, last is refl: BACDa> | <Pin sets: TRDC> | [<Start k: KSFT>, <Type k: LKMN>] | text'
+      String => 'BACDA TRDC KSFT LKMN | text' // Each character group can be replaced to '*' and system will generate a random setting
       Obj => {
         eng: 'BACDA',
-        pin: 'TRDC',
+        pin: 'TRDC',  // Its necesary to fill this one although its useless. Im thinking in how it should works, seems hard to program without crashing all 
         sk: 'KSFT',
-        tk: 'LKMN', // This is working how it should but need to make an automatic process to generate passwords
+        tk: 'LKMN', // This should be typed only if you are decrypting, else its recomended to use the '*' to generate a random tk
       }
     */
 
     // Everything here has been moved to /lib/objects/enigma/pieces.js
 
+    // This line right here executes whenever the input doesnt includes '|'
     if (sets.eng === undefined) noset = true
-    // Ill try to make a system to generate passwords to ppl that wants security but are lazy
 
     if (_test) {
       let res = runner(inT, sets, noset)
